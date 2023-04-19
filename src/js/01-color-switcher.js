aae485338b784e2fae76ced1d4a24d15c056a685
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-19 22:17:39 +0200

    Завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+const isColorChanging = false;
+
+startButton.addEventListener('click', () => {
+    if (!isColorChanging) {
+        isColorChanging = true;
+        startButton.disabled = true;
+        const intervalId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        stopButton.addEventListener('click', () => {
+            clearInterval(intervalId);
+            isColorChanging = false;
+            startButton.disabled = false;
+        })
+    }
+});

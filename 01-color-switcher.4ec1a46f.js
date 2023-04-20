f27e9dde3b0155dc82b09e206ff10d6644235eed
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-20 12:38:59 +0000

    Deploying to gh-pages from @ ViktoriiaKN/goit-js-hw-09@2e0ad555ef9835e649d47f3459c98e458a52ff69 🚀

---
+++ 01-color-switcher.4ec1a46f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0;var e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);n.addEventListener("click",(function(){clearInterval(e),startButton.disabled=!1}))}))}();
+//# sourceMappingURL=01-color-switcher.4ec1a46f.js.map

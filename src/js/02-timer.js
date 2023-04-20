21243ac939019126d7ef8174e922e6d2d5623cfb
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-20 21:24:49 +0200

    Styles.css

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,4 +2,9 @@
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+// Додаткова бібліотека
+import Notiflix from 'notiflix';
+
+const dateInput = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
 

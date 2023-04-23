e6d2cd703db774d425fc64e195de39f1ba441bfc
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-23 21:25:20 +0200

    Початок 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputForm = document.querySelector('input');
+const submitBtn = document.querySelector('button');
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm() {
+  
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +18,4 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+

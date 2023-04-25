f6d2689fcf6739b630f68243ea19ea263e3da684
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-25 16:02:20 +0200

    Завдання 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,46 @@
+// Import library
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const inputForm = document.querySelector('input');
-const submitBtn = document.querySelector('button');
+// Get form element
 const formRef = document.querySelector('.form');
 
+// Set event listener submit on form
 formRef.addEventListener('submit', onSubmitForm);
 
-function onSubmitForm() {
-  
-}
+// Submit form
+function onSubmitForm(e) {
+  e.preventDefault();
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  let delay = Number(formRef.delay.value);
+
+  for (let i = 1; i <= formRef.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formRef.step.value);
   }
 }
+
+// Create promise
+function createPromise(position, delay) {
+  const obj = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(obj);
+      } else {
+        // Reject
+        reject(obj);
+      }
+    }, delay);
+  });
+}
+
 
